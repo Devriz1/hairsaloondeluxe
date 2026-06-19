4964b137801d6c0f2ed12c5d57d5ac703a304c05
Devriz1 <risalrichu01@gmail.com>
2026-06-20 01:55:15 +0530

    new update

--- js/app.js
+++
@@ -1,9 +0,0 @@
-// Example: WhatsApp button click
-document.querySelector(".btn-white")?.addEventListener("click", () => {
-  window.open("https://wa.me/your-number", "_blank");
-});
-
-// Call button
-document.querySelector(".btn-glass")?.addEventListener("click", () => {
-  window.location.href = "tel:+15551234567";
-});
